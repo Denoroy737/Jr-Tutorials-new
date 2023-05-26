7a6a8f4ae4e729b10f92bf03990704f70875b815
Kena737 <singhamitsboss@gmail.com>
2023-05-26 14:38:56 +0530

    have to take risk

--- pages/Course/[slug].tsx
+++ pages/Course/[slug].tsx
@@ -1,9 +1,9 @@
-import { GetStaticProps, GetStaticPaths } from 'next'
-import { useRouter } from 'next/router'
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { useRouter } from 'next/router';
 import { Course } from '../../typings';
 import Playlist from './playlist';
 import { sanityClient, urlFor } from '../../sanity';
-import "node_modules/video-react/dist/video-react.css";
+import 'video-react/dist/video-react.css';
 import { Player } from 'video-react';
 
 interface Props {
@@ -11,26 +11,24 @@ interface Props {
 }
 
 const PostPage = ({ course }: Props) => {
-  console.log(course);
+  console.log("course", course);
   const router = useRouter();
 
   if (router.isFallback) {
     return <div>Loading...</div>;
   }
-
   return (
     <div className='my-5 flex md:justify-between md:flex-row flex-col md:space-x-5'>
       <div className='border-b border-[#0e0e0e]'>
         <span className='text-lg text-white font-bold'>COURSE</span>
-        <div className=' my-5'>
+        <div className='my-5'>
           {/* <Image src={urlFor(course.mainImage).url()} alt="Thumbnail" width={700} height={400} className="rounded-xl" /> */}
-          {course.videos && course.videos.length > 0 && (
-            <Player
-              playsInline
-              poster={urlFor(course.mainImage).width(900).url()}
-              src={course.videos[0]?.video?.secure_url} // Added optional chaining '?'
-            />
-          )}
+          <Player
+            playsInline
+            poster={urlFor(course.mainImage).width(900).url()}
+            src={course.videos[0].video.secure_url} // Accessing the secure_url property of the video object
+          />
+
           <div className='my-3'>
             <div className="flex space-x-3">
               <button className='border-b-2 border-gray-600 text-white md:px-2 md:font-medium '>Overview</button>
@@ -59,7 +57,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const query = `*[_type == "Courses" && slug.current == $slug][0]`
 
-  const course = await sanityClient.fetch(query, { slug })
+  const course = await sanityClient.fetch<Course>(query, { slug })
 
   return {
     props: { course },
@@ -69,9 +67,9 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const query = `*[_type == "Courses"]{slug}`
-  const courses = await sanityClient.fetch(query)
+  const courses = await sanityClient.fetch<{ slug: { current: string } }[]>(query)
 
-  const paths = courses.map((course: { slug: { current: string } }) => ({
+  const paths = courses.map((course) => ({
     params: { slug: course.slug.current }
   }))
 
--- typings.d.ts
+++ typings.d.ts
@@ -8,77 +8,75 @@ interface SanityDocument {
 
 // Path: src/types/course.ts
 export interface Course extends SanityDocument {
-
     _type: "Courses";
     title: string;
     slug: {
-        _type: "slug";
-        current: string;
+      _type: "slug";
+      current: string;
     };
     body: string;
     class: number;
     price: number;
     description: string;
     videos: {
-        length: number;
-        _key: string;
-        _type: "video";
-        description: string;
-        chapter: number;
-        lecture: number;
-        mainImage: {
-            _type: "image";
-            asset: {
-                _ref: string;
-                _type: "reference";
-            };
-        };
-        title: string;
-        video: {
-            _key: string;
-            _type: "cloudinary.asset";
-            access_control: never[];
-            access_mode: "public";
-            bytes: number;
-            created_at: string;
-            created_by: {
-                _type: "reference";
-                _ref: string;
-            };
-            duration: number;
-            format: "mp4";
-            height: number;
-            metadata: never[];
-            public_id: string;
-            resource_type: "video";
-            secure_url: string;
-            tags: never[];
-            type: "upload";
-            uploaded_by: {
-                _type: "reference";
-                _ref: string;
-            };
-            url: string;
-            version: number;
-            width: number;
-        };
-    };
-    mainImage: {
+        
+      _key: string;
+      _type: index;
+      description: string;
+      chapter: number;
+      lecture: number;
+      mainImage: {
         _type: "image";
         asset: {
-            _ref: string;
-            _type: "reference";
+          _ref: string;
+          _type: "reference";
         };
+      };
+      title: string;
+      video: {
+        _key: string;
+        _type: "cloudinary.asset";
+        access_control: never[];
+        access_mode: "public";
+        bytes: number;
+        created_at: string;
+        created_by: {
+          _type: "reference";
+          _ref: string;
+        };
+        duration: number;
+        format: "mp4";
+        height: number;
+        metadata: never[];
+        public_id: string;
+        resource_type: "video";
+        secure_url: string;
+        tags: never[];
+        type: "upload";
+        uploaded_by: {
+          _type: "reference";
+          _ref: string;
+        };
+        url: string;
+        version: number;
+        width: number;
+      };
+    };
+    mainImage: {
+      _type: "image";
+      asset: {
+        _ref: string;
+        _type: "reference";
+      };
     };
     author: {
-        _ref: string;
-        _type: "reference";
+      _ref: string;
+      _type: "reference";
     };
     categories: {
-        _key: string;
-        _ref: string;
-        _type: "reference";
+      _key: string;
+      _ref: string;
+      _type: "reference";
     }[];
-
-
-};
+  }
+  
